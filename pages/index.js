eacbe60cc4906fc3ae1cf455e1ce856494dc3d2f
mrima <mrimamss@gmail.com>
2023-04-09 17:28:52 +0300

    add styles

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
     return (
      <>
          <div className={"relative"}>
-             <Canvas style={{background: "red", width: "100vw", height: "100vh",  backgroundImage: "radial-gradient(circle farthest-side at 70% 45%, #11131c, #030304, #0a0b11)"}} shadows>
+             <Canvas style={{background: "red", width: "100vw", height: "100vh",  backgroundImage: "radial-gradient(circle farthest-side at 70% 45%, #11131c, #030304, #0a0b11)",  backgroundSize: "100% 100%", backgroundPosition: "left center", backgroundRepeat: "no-repeat"}} shadows>
                  <Suspense fallback={null}>
                      <Three/>
                  </Suspense>
