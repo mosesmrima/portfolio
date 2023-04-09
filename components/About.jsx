623e5ee8b47865702f46502acff86ed478bb0956
mrima <mrimamss@gmail.com>
2023-04-09 17:17:55 +0300

    about structure

--- components/About.jsx
+++ components/About.jsx
@@ -20,15 +20,17 @@ export default function About () {
             delay: 0.5,
             scrollTrigger: {
                 trigger: ".hello",
-                start: "bottom bottom",
-                scrub: true
+                start: "bottom bottom"
             },
         })
     }, [])
     return (
-        <div className={"about h-screen flex justify-center items-center"}>
-            <div className={"hello max-w-[350px] bg-blue-400 h-auto"}>
-                01 01 01 00 10 10 10 01 01 01 01 00 10 01 01 01 01 00 10 10 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+        <div className={"about p-8 flex flex-col justify-center items-center"}>
+            <h1 className={"text-white bg-red-100 justify-self-start"} >About Me</h1>
+            <div className={"flex justify-center items-center"}>
+                <div className={"hello max-w-[350px] bg-blue-400 h-auto"}>
+                    01 01 01 00 10 10 10 01 01 01 01 00 10 01 01 01 01 00 10 10 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+                </div>
             </div>
         </div>
     )
