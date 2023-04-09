2f1dca353c9b9bebcb6e9666a494db1e3e8b3310
mrima <mrimamss@gmail.com>
2023-04-09 15:37:25 +0300

    initial about page

---
+++ components/About.jsx
@@ -0,0 +1,35 @@
+import { gsap } from "gsap/dist/gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import {TextPlugin} from  "gsap/dist/TextPlugin"
+import {useEffect} from "react";
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin)
+
+export default function About () {
+
+
+    useEffect(()=>{
+        gsap.to(".hello", {
+            text: {
+                value: "Hello, Im Mrima, a software engineer by day and I track ransomware gangs and reverse engineer malware by night",
+            },
+            duration: 10,
+            delay: 0.5,
+            scrollTrigger: {
+                trigger: ".hello",
+                start: "bottom bottom",
+                scrub: true
+            },
+        })
+    }, [])
+    return (
+        <div className={"about h-screen flex justify-center items-center"}>
+            <div className={"max-w-[350px] bg-blue-400 h-auto"}>
+                01 01 01 00 10 10 10 01 01 01 01 00 10 01 01 01 01 00 10 10 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01
+            </div>
+        </div>
+    )
+}
