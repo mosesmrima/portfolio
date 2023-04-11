021217080195ff2476025711303f170744c6f452
mrima <mrimamss@gmail.com>
2023-04-11 08:02:56 +0300

    add descriptions

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ export default function About () {
 
         gsap.to(".hello", {
             text: {
-                value: "Hello, Im Mrima, a software engineer by day and I track ransomware gangs and reverse engineer malware by night",
+                value: "I am a cyber security researcher and software engineer. With a passion for both fields, I have been continually developing my skills in reverse engineering malware and performing threat intelligence, including tracking ransomware gangs. As a software engineer, I specialize in building user-friendly and scalable applications with security in mind.",
             },
             duration: 3,
             delay: 2,
@@ -27,13 +27,13 @@ export default function About () {
     }, [])
     return (
         <div className={"about p-8"}>
-            <h1 className={`${roboto.variable} font-extrabold sm:text-4xl text-3xl text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500 text-center w-full mx-auto p-12`} >About Me</h1>
+            <h1 className={`${roboto.variable} font-extrabold sm:text-4xl text-3xl text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500 text-center w-full mx-auto p-12`} >$whomai</h1>
             <div className={"flex justify-around items-center p-8 md:flex-row flex-col"}>
              <div className={"bg-gradient-to-tl from-white to-neutral-500 rounded-2xl p-0.5"}>
                  <div className={"about-text rounded-2xl"}>
                      <div className={"bg-white bg-opacity-5 backdrop-blur-lg max-w-[350px] sm:min-w-[450px] min-w-[250px] min-h-[200px] h-auto rounded-2xl p-4 flex justify-center items-center"}>
                          <p className={"hello text-lg text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-300"}>
-                             010101 10 010101 1 10101010 01010101 10 010 010 0 01010 0101010101 10101 101 1010101 10101010 0101010 01 10101
+                             0 01 1 1010101010101 1010101010 010 01010101 101010101 1010 0 0101010 010 0101 1010101 1 1010 0101 10101010101 1010101010 01 101010 01 1010101 10101010101 1010101 101 1010101010 010101 1010101010101 101010101 10101010 0101010101 101010 01 1 10101010 010101010 0 0101010101 10 01010101 1010101010101 101 10101010 010101010101 1010 01010101 10 01010
                          </p>
                      </div>
                  </div>
