30b4089f03c46c6807579bf57e3d72c48657fa80
mrima <mrimamss@gmail.com>
2023-04-14 13:34:12 +0300

    first view

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,21 +1,26 @@
 import {roboto, pressStart2P} from "@/pages";
 import { Carousel} from 'flowbite-react';
 import {FiArrowLeftCircle, FiArrowRightCircle} from "react-icons/fi"
+import Image from "next/image";
+import vigil from "/public/vigil.png"
 
 export default function Projects() {
 
     return (
         <div className={`projects p-8 flex flex-col justify-center items-center`}>
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 sm:w-[75vw] w-[90vw] max-w-[800px]">
+            <h2 className={`${pressStart2P.variable} font-serif sm:text-3xl text-2xl text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500 text-center w-full mx-auto p-4`} >portfolio</h2>
+            <div className="h-[350px] sm:h-64 xl:h-80 2xl:h-96 sm:w-[75vw] w-[90vw] max-w-[800px]">
                 <Carousel className={"sm:w-[75vw] w-[90vw] max-w-[800px]"} slideInterval={6000}  leftControl={<FiArrowLeftCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} rightControl={<FiArrowRightCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} >
-                    <div className="flex h-full items-center justify-center bg-yellow-400 w-full">
-                        Slide 1
-                    </div>
-                    <div className="flex h-full items-center justify-center bg-green-400 w-full">
-                        Slide 2
-                    </div>
-                    <div className="flex h-full items-center justify-center bg-red-400 w-full">
-                        Slide 3
+                    <div className="flex flex-col h-full items-center justify-center items center bg-zinc-300 bg-opacity-10 rounded-2l border border-zinc-500 w-full p-4">
+                        <h2 className={`${pressStart2P.variable} font-serif sm:text-xl text-lg text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500`}>Vigil</h2>
+                        <div className={"w-11/12 h-full flex sm:flex-row flex-col gap-4 justify-center items-center"}>
+                            <div>
+                                <Image src={vigil} alt={"vigil"} className={"rounded-xl"}/>
+                            </div>
+                            <p className={`${roboto.variable} font-sans text-base text-transparent bg-clip-text bg-gradient-to-tl from-neutral-400 to-white`}>
+                                A cyber threat intelligence dashboard and API to track significant cyber security incidents.
+                            </p>
+                        </div>
                     </div>
 
                 </Carousel>
