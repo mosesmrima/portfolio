2f1cba7b67fab45c91cb32e24f8f97bdc866ef97
mrima <mrimamss@gmail.com>
2023-04-14 14:15:56 +0300

    hide indicators

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
         <div className={`projects p-8 flex flex-col justify-center items-center`}>
             <h2 className={`${pressStart2P.variable} font-serif sm:text-3xl text-2xl text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500 text-center w-full mx-auto p-4`} >portfolio</h2>
             <div className="h-[350px] sm:h-64 xl:h-80 2xl:h-96 sm:w-[75vw] w-[90vw] max-w-[800px]">
-                <Carousel className={"sm:w-[75vw] w-[90vw] max-w-[800px]"} slideInterval={6000}  leftControl={<FiArrowLeftCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} rightControl={<FiArrowRightCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} >
+                <Carousel indicators={false} className={"sm:w-[75vw] w-[90vw] max-w-[800px]"} slideInterval={6000}  leftControl={<FiArrowLeftCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} rightControl={<FiArrowRightCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} >
                     <div className="flex flex-col h-full items-center justify-center items center bg-zinc-300 bg-opacity-10 rounded-2l border border-zinc-500 w-full p-4">
                         <h2 className={`${pressStart2P.variable} font-serif sm:text-xl text-lg text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500`}>Vigil</h2>
                         <div className={"w-11/12 h-full flex sm:flex-row flex-col gap-4 justify-center items-center"}>
