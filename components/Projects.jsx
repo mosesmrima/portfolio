fd00149bc86d5a9e71d16a7b47a69866da868111
mrima <mrimamss@gmail.com>
2023-04-14 14:14:29 +0300

    change width

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -47,7 +47,7 @@ export default function Projects() {
                     <div className="flex flex-col h-full items-center justify-center items center bg-zinc-300 bg-opacity-10 rounded-2l border border-zinc-500 w-full p-4">
                         <h2 className={`${pressStart2P.variable} font-serif sm:text-xl text-lg text-transparent bg-clip-text bg-gradient-to-tl from-white to-neutral-500`}>Kentronix</h2>
                         <div className={"w-11/12 h-full flex sm:flex-row flex-col gap-4 justify-center items-center"}>
-                            <div className={"w-[50%]"}>
+                            <div className={"sm:w-[50%]"}>
                                 <Image src={kentronix} alt={"kentronix"} className={"rounded-xl"}/>
                             </div>
                             <div className={"flex flex-col justify-between items-center"}>
