475af8c43c99a6ca1e71d9fdc990b4c49b1f01d9
mrima <mrimamss@gmail.com>
2023-04-14 12:12:17 +0300

    change width

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -6,15 +6,15 @@ export default function Projects() {
 
     return (
         <div className={`projects p-8 flex flex-col justify-center items-center`}>
-            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-                <Carousel auto={true} infinite={true} transition={"fade"} className={"sm:w-[75vw] w-[90vw] max-w-[800px]"} slideInterval={6000}  leftControl={<FiArrowLeftCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} rightControl={<FiArrowRightCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} >
-                    <div className="flex h-full items-center justify-center bg-yellow-400 w-10/12">
+            <div className="h-56 sm:h-64 xl:h-80 2xl:h-96 sm:w-[75vw] w-[90vw] max-w-[800px]">
+                <Carousel className={"sm:w-[75vw] w-[90vw] max-w-[800px]"} slideInterval={6000}  leftControl={<FiArrowLeftCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} rightControl={<FiArrowRightCircle size={30} style={{ stroke: "url(#blue-gradient)" }}/>} >
+                    <div className="flex h-full items-center justify-center bg-yellow-400 w-full">
                         Slide 1
                     </div>
-                    <div className="flex h-full items-center justify-center bg-green-400 w-10/12">
+                    <div className="flex h-full items-center justify-center bg-green-400 w-full">
                         Slide 2
                     </div>
-                    <div className="flex h-full items-center justify-center bg-red-400 w-10/12">
+                    <div className="flex h-full items-center justify-center bg-red-400 w-full">
                         Slide 3
                     </div>
                 </Carousel>
